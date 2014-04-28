7c085a5d87106c828b86b8345bc874a2338c3786
木村 大智 <d.kimura@leihauoli.com>
2014-04-28 10:58:40 +0900

    initial commit

---
+++ Boxstarter.ps1
@@ -0,0 +1,96 @@
+## system configuration
+
+	Disable-UAC
+	Enable-RemoteDesktop
+	Install-WindowsUpdate -AcceptEula
+	Set-ExplorerOptions -showHidenFilesFoldersDrives -showFileExtensions
+	Set-TaskbarSmall
+	Update-ExecutionPolicy Unrestricted
+
+## install application
+
+### browser
+
+	cinst GoogleChrome
+	cinst chromium
+	cinst GoogleChrome.Canary
+	cinst Firefox
+	cinst Opera
+
+### web debugging tool
+
+	cinst fiddler4
+	cinst wireshark
+	cinst PhantomJS
+	cinst casperjs
+
+### editor
+
+	cinst sublimetext2
+	cinst WebStorm
+	cinst notepadplusplus
+	cinst eclipse-standard-kepler
+	cinst Brackets
+	cinst markdownpad2
+
+### version control system
+
+	cinst git
+	cinst gitextensions
+	cinst SourceTree
+	cinst githubforwindows
+	cinst Devbox-GitFlow
+	cinst TortoiseGit
+	cinst svn
+	cinst tortoisesvn
+
+### merge tool
+
+	cinst winmerge-jp
+	cinst kdiff3
+
+### archiver
+
+	cinst 7zip
+	cinst winrar
+	cinst lhaplus
+
+### language
+
+	cinst ruby
+	cinst rubygems
+	cinst python
+	cinst pip
+	cinst nodejs
+
+### ftp, sftp and ssh client
+
+	cinst winscp
+	cinst putty
+
+### command line tool
+
+	cinst ConsoleZ
+	cinst Gow
+
+### virtual machine
+
+	cinst virtualbox
+	cinst vagrant
+
+### image
+
+	cinst IrfanView
+
+### apache distribution
+
+	cinst XAMPP
+
+### utility
+
+	cinst ccleaner
+	cinst QTTabBar
+
+## finish message
+
+	Write-BoxstarterMessage 'Finished'
